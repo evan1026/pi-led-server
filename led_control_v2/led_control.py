7dd0bd83c7f8e2b871fabe4808f43e22ba713fb6
Evan Allan <evanallan1026@gmail.com>
2024-09-11 03:01:38 -0500

    Basic setup of a pattern, already liking it better

--- led_control_v2/led_control.py
+++ led_control_v2/led_control.py
@@ -1,8 +1,10 @@
+import math
 from typing import Dict
 
 from rpi_ws281x import PixelStrip
 
 from .command_handling import CommandHandler
+from .pattern import NothingPattern, ColorPattern, FullRandomPattern
 
 LED_COUNT = 900
 LED_PIN = 18
@@ -20,14 +22,15 @@ class LedStrip(PixelStrip):
 
 commands: Dict[str, CommandHandler] = {
     'set_brightness': lambda args: None,
-    'set_color': lambda args: None,
+    'set_color': lambda args: ColorPattern(args[0]),
     'set_scale_factor': lambda args: None,
     'halloween1': lambda args: None,
     'halloween2': lambda args: None,
-    'halloween3': lambda args: None
+    'halloween3': lambda args: None,
+    'full_random': lambda args: FullRandomPattern()
 }
 
-no_args_commands = ['halloween1', 'halloween2', 'halloween3']
+no_args_commands = ['halloween1', 'halloween2', 'halloween3', 'full_random']
 set_value_commands = ['set_brightness', 'set_scale_factor']
 
 
@@ -39,12 +42,19 @@ def run_control_loop(pipe):
     strip.begin()
     strip.show()
 
-    current_pattern = None  # TODO suitable default
+    current_pattern = NothingPattern()
+    current_progress = 0
+    progress_increment = 0.01
     try:
         while True:
-            new_pattern = process_command(commands, pipe, wait=True)
+            new_pattern = process_command(commands, pipe, wait=False)
             current_pattern = new_pattern if new_pattern is not None else current_pattern
-            # TODO run current pattern
+
+            current_pattern.run(strip, current_progress)
+            strip.show()
+
+            current_progress += progress_increment
+            current_progress = current_progress - math.trunc(current_progress)
     except KeyboardInterrupt:
         pass
     finally:
--- led_control_v2/pattern.py
+++ led_control_v2/pattern.py
@@ -1,2 +1,31 @@
-class Pattern:
-    pass
+from abc import ABC, abstractmethod
+from random import randint
+
+from rpi_ws281x import PixelStrip, RGBW, Color
+
+
+class Pattern(ABC):
+    @abstractmethod
+    def run(self, strip: PixelStrip, progress: float):
+        pass
+
+
+class NothingPattern(Pattern):
+    def run(self, strip: PixelStrip, progress: float):
+        pass
+
+
+class ColorPattern(Pattern):
+    def __init__(self, color: RGBW):
+        self.color = color
+
+    def run(self, strip: PixelStrip, progress: float):
+        for i in range(strip.numPixels()):
+            strip.setPixelColor(i, self.color)
+
+
+class FullRandomPattern(Pattern):
+    def run(self, strip: PixelStrip, progress: float):
+        for i in range(strip.numPixels()):
+            def randi(): return randint(0, 255)
+            strip.setPixelColor(i, Color(randi(), randi(), randi()))
